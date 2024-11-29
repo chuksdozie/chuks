a715cf2cad657bec90fe4ad3300524b259358e3c
chuks dozie <chuksdozie48@gmail.com>
2024-11-29 23:45:04 +0100

    Feat: Device Info Custom Native Android Module

---
+++ android/app/src/main/java/com/chuks/CustomReactPackage.java
@@ -0,0 +1,24 @@
+package com.chuks;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CustomReactPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new DeviceInfoModule(reactContext));
+        return modules;
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
--- src/screens/home/UserHome.tsx
+++ src/screens/home/UserHome.tsx
@@ -1,15 +1,25 @@
 import {View, ScrollView} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import GeneralLayout from '../../layouts/GeneralLayout';
 import tw from 'twrnc';
 import colors from '../../themes/colors';
 import DefaultScreenHeader from '../../components/shared/DefaultScreenHeader';
+import {getDeviceModel} from '../../utils/DeviceInfo';
 
 interface Props {
   name?: string;
 }
 
 const UserHome: React.FC<Props> = () => {
+  useEffect(() => {
+    const fetchDeviceInfo = async () => {
+      const model = await getDeviceModel();
+      console.log({model});
+      // setDeviceModel(model);
+    };
+    fetchDeviceInfo();
+  }, []);
+
   return (
     <GeneralLayout style={tw`px-0`}>
       <View style={tw` bg-[${colors.gray25}] flex-1`}>
